cc4c2542e0e7686b1f350cbf41f5da8c14c508d3
sipl_103 <sipl_103@sipl-103s-iMac.local>
2012-02-18 00:16:40 +0530

    Initial Commit

---
+++ CalanderView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CalanderView
+//
+//  Created by sipl_103 on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CalanderView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CalanderView
+//
+//  Created by sipl_103 on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
